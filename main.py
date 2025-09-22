93981e45ac6d483b41e9497259ce96c78c93f12b
UmmeHabiba1312 <ibrahimr1312@gmail.com>
2025-09-22 15:41:24 +0500

    add new port

--- main.py
+++ main.py
@@ -347,5 +347,7 @@ async def start_chat(req: ChatRequest):
 
 
 if __name__ == "__main__":
-    import uvicorn  
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    import uvicorn
+    port = int(os.getenv("PORT", 8080))   
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
